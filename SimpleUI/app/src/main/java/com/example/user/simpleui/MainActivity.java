43ec5f45b019dd4fafecf8a0b0f25836d66d0ce6
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 11:17:33 +0800

    Edit Radio Button

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,22 +3,47 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+
+    String sex = "male";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.maleRadioButton)
+                {
+                    sex = "Male";
+                }
+                else if(checkedId == R.id.femaleRadioButton)
+                {
+                    sex = "Female";
+                }
+            }
+        });
     }
 
     public void submit(View view)
     {
-        textView.setText("Hello Android");
+        String text = editText.getText().toString();
+        text = text + " Sex:" + sex;
+        textView.setText(text);
+        editText.setText("");
     }
 }
